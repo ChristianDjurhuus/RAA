bda7d8774ceffcc24b2eae00274970641b76b30c
ChristianDjurhuus <christian.v.djurhuus@gmail.com>
2022-02-09 13:13:02 +0100

    setting up cookie cutter format

---
+++ RAA/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Implementation of Relational Archetypal Analysis',
+    author='Christian Djurhuus, Daniel Holmelund, Felix Burmester',
+    license='MIT',
+)
