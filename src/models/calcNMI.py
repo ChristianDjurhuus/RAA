2c205941df0de2b4d3740d939f7e91adbd9a715f
Ne0-1 <57833596+Ne0-1@users.noreply.github.com>
2022-04-13 10:58:56 +0200

    Done with mi

--- src/models/calcNMI.py
+++ src/models/calcNMI.py
@@ -1,6 +1,7 @@
 #######################
 ## Mutual Information##
 #######################
+import numpy as np
 import torch
 
 def calcMI(Z1, Z2):
@@ -14,13 +15,16 @@ def calcMI(Z1, Z2):
         MI Mutual information between Z1 and Z2
     Inspired from source code provided by Morten Mørup
     '''
+    if Z1.shape[1] != Z2.shape[1]:
+        print('OOPS! Number of columns need to be equal in the two matrices! (Try transposing one of them)')
+        return np.nan
     
     P=Z1@Z2.T
     PXY=P/torch.sum(torch.sum(P))
-    PXPY=torch.sum(PXY,1)@torch.sum(PXY,0)
+    PXPY=torch.sum(PXY,1).unsqueeze(1) * torch.sum(PXY,0)
     ind=torch.where(PXY>0)
     MI=sum(PXY[ind]*torch.log(PXY[ind]/PXPY[ind]))
-    return 
+    return MI
 
 def calcNMI(Z1, Z2):
     '''
@@ -34,7 +38,10 @@ def calcNMI(Z1, Z2):
     Inspired from source code provided by Morten Mørup
     ''' 
     return 2 * calcMI(Z1, Z2) / (calcMI(Z1, Z1) + calcMI(Z2, Z2))
-
+torch.random.manual_seed(0)
 Z1 = torch.rand((10, 5))
 Z2 = torch.rand((10, 10))
+Z1 = Z1.T #Need number of columns to be equal, not rows <3
+Z2 = Z2.T
 print(calcNMI(Z1, Z1))
+print(calcMI(Z1,Z2)) #Gives 0.026 as Mortens matlab solution!
