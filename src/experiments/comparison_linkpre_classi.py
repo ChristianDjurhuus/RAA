277fa16cdaa15b04803c8c8d9a8973bfa88a687f
Laitallis <danielholmelund@hotmail.com>
2022-04-16 11:20:35 +0200

    bug fix

--- src/experiments/comparison_linkpre_classi.py
+++ src/experiments/comparison_linkpre_classi.py
@@ -20,12 +20,18 @@ n_archetypes = torch.arange(2,11)
 seeds = torch.randint(low = 0, high = 10000, size = (10,))
 # Outer loop for the three dataset
 dataset = ['data/raw/karate/karate.gml', 'data/raw/dolphin/dolphins.gml''data/raw/polblogs/polblogs.gml']
-data_names = ["karate", "dolphins", "polblogs"]
+data_names = ["Karate", "Dolphins", "Polblogs"]
 # TODO slight problem. Dolphins have no group metadata...
 attribute_list = ["club", "label", "value"]
 
 # Create dict for AA AUC score
 AA_AUC_scores = {key: [] for key in data_names}
+LSM_AUC_score_plot = {key: [] for key in data_names}
+LSM_KNN_plot = {key: [] for key in data_names}
+LSM_LR_plot = {key: [] for key in data_names}
+AA_KNN_plot = {key: [] for key in data_names}
+AA_LR_plot = {key: [] for key in data_names}
+
 for i, data in tqdm(enumerate(dataset)):
     # Train 10 LSM models and take the best
     LSM_AUC_score, LSM_KNN, LSM_LR = [], [], []
@@ -37,6 +43,7 @@ for i, data in tqdm(enumerate(dataset)):
                 data_type = "gml")
         model.train(iterations = iterations, LR = 0.01, print_loss = False)
         LSM_AUC_score_temp, _, _ = model.link_prediction()
+        LSM_AUC_score.append(LSM_AUC_score)
         knn_score = model.KNeighborsClassifier(attribute = attribute_list[i])
         LSM_KNN.append(knn_score)
         lr_score = model.logistic_regression(attribute = attribute_list[i])
@@ -46,12 +53,15 @@ for i, data in tqdm(enumerate(dataset)):
     # Find the best LSM based on lowest loss
     min_LSM_loss = min(LSM_losses)
     min_LSM_loss_idx = LSM_losses.index(min_LSM_loss)
+    LSM_AUC_score = [LSM_AUC_score[min_LSM_loss_idx] for x in range(len(n_archetypes))]
     LSM_KNN = [LSM_KNN[min_LSM_loss_idx] for x in range(len(n_archetypes))]
     LSM_LR = [LSM_LR[min_LSM_loss_idx] for x in range(len(n_archetypes))]
+    # append to the plotting dict
+    LSM_AUC_score_plot[data_names[i]].append(LSM_AUC_score)
+    LSM_KNN_plot[data_names[i]].append(LSM_KNN)
+    LSM_LR_plot[data_names[i]].append(LSM_LR)
     torch.random.manual_seed(seeds[min_LSM_loss_idx]) # Train AA with same seed
 
-    AA_KNN = []
-    AA_LR = []
     for idx in range(len(n_archetypes)):
         model = DRRAA(data = data,
                         k=n_archetypes[idx],
@@ -64,24 +74,25 @@ for i, data in tqdm(enumerate(dataset)):
         
 
         knn_score = model.KNeighborsClassifier(attribute = attribute_list[i])
-        AA_KNN.append(knn_score)
+        AA_KNN_plot[data_names[i]].append(knn_score)
         lr_score = model.logistic_regression(attribute = attribute_list[i])
-        AA_LR.append(lr_score)
+        AA_LR_plot[data_names[i]].append(lr_score)
 
 LSM_AUC_scores = [LSM_AUC_score for x in range(len(n_archetypes))]
 
 fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(20,10), dpi=400)
-ax1.plot(LSM_AUC_scores, label = "LSM AUC")
 for data_name in data_names:
-    ax1.plot(AA_AUC_scores[data_name], label = data_name)
+    ax1.plot(LSM_AUC_scores, label = data_name + "LSM AUC")
+    ax1.plot(AA_AUC_scores[data_name], label = data_name + "AA AUC")
 ax1.set_xticks(n_archetypes.numpy())
 ax1.set_title(f"DRRAA vs. LSM with {iterations} iterations")
 ax1.set_ylabel("AUC-score")
 
-ax2.plot(LSM_KNN, label = "LSM KNN")
-ax2.plot(LSM_LR, label = "LSM LR")
-ax2.plot(AA_KNN, label = "AA KNN")
-ax2.plot(AA_LR, label = "AA LR")
+for data_name in data_names:
+    ax2.plot(LSM_KNN_plot[data_name], label = data_name + "LSM KNN")
+    ax2.plot(LSM_LR_plot[data_name], label = data_name + "LSM LR")
+    ax2.plot(AA_KNN_plot, label = data_name + "AA KNN")
+    ax2.plot(AA_LR_plot, label = data_name + "AA LR")
 ax2.set_xticks(n_archetypes.numpy())
 ax2.set_title("Node classification")
 ax2.set_ylabel("Score")
