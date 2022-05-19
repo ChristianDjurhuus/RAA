371a9c8e43491dfc5cfaa34832b5b9958edf2f4f
Ne0-1 <57833596+Ne0-1@users.noreply.github.com>
2022-05-19 12:19:06 +0200

    update raa_gate

--- src/models/train_RAA_gate.py
+++ src/models/train_RAA_gate.py
@@ -10,14 +10,22 @@ import seaborn as sns
 from torch_sparse import spspmm
 import numpy as np
 
-class RAA(nn.Module):
-    def __init__(self, A, edge_list, input_size, k, sample_size, sampling_weights):
+from src.visualization.visualize import Visualization
+from src.features.link_prediction import Link_prediction
+from src.features.preprocessing import Preprocessing
+
+class RAA(nn.Module, Link_prediction, Preprocessing, Visualization):
+    def __init__(self, A, edge_list, input_size, k, sample_size, sampling_weights, data_type='edge list', test_size=0.3):
         super(RAA, self).__init__()
         self.A = A
         self.edge_list = edge_list
         self.input_size = input_size
         self.k = k
-
+        self.test_size = test_size
+        self.data_type = data_type
+        Preprocessing.__init__(self, data=data, data_type=data_type, device=self.device, data_2=data_2)
+        self.edge_list, self.N, self.G = Preprocessing.convert_to_egde_list(self)
+        Link_prediction.__init__(self)
         self.beta = torch.nn.Parameter(torch.randn(self.input_size[0]))
         self.softplus = nn.Softplus()
         self.a = torch.nn.Parameter(torch.randn(1))
@@ -81,143 +89,3 @@ class RAA(nn.Module):
         log_likelihood_sparse = z_pdist2 - z_pdist1
         return log_likelihood_sparse
     
-    def link_prediction(self, A_test, idx_i_test, idx_j_test):
-        with torch.no_grad():
-            Z = F.softmax(self.Z, dim=0)
-            G = F.sigmoid(self.G)
-            C = (Z.T * G) / (Z.T * G).sum(0) #Gating function
-
-            M_i = torch.matmul(torch.matmul(Z, C), Z[:, idx_i_test]).T #Size of test set e.g. K x N
-            M_j = torch.matmul(torch.matmul(Z, C), Z[:, idx_j_test]).T
-            z_pdist_test = ((M_i - M_j + 1e-06)**2).sum(-1)**0.5 # N x N 
-            theta = (self.beta[idx_i_test] + self.beta[idx_j_test] - self.a * z_pdist_test) # N x N
-
-            #Get the rate -> exp(log_odds) 
-            rate = torch.exp(theta) # N
-
-            #Create target (make sure its in the right order by indexing)
-            target = A_test[idx_i_test, idx_j_test] #N
-
-
-            fpr, tpr, threshold = metrics.roc_curve(target.numpy(), rate.numpy())
-
-
-            #Determining AUC score and precision and recall
-            auc_score = metrics.roc_auc_score(target.cpu().data.numpy(), rate.cpu().data.numpy())
-
-            return auc_score, fpr, tpr
-
-
-if __name__ == "__main__": 
-    seed = 4
-    torch.random.manual_seed(seed)
-
-    #A = mmread("data/raw/soc-karate.mtx")
-    #A = A.todense()
-    ZKC_graph = nx.karate_club_graph()
-    #Let's keep track of which nodes represent John A and Mr Hi
-    Mr_Hi = 0
-    John_A = 33
-
-    # Get the edge list
-    edge_list = np.array(list(map(list, ZKC_graph.edges()))).T
-    # edge_list.sort(axis=1)  #TODO: Sort in order to recieve the upper triangular part of the adjacency matrix
-    edge_list = torch.from_numpy(edge_list).long()
-
-    # Get N and latent_dim (k)
-    N = len(ZKC_graph.nodes())
-    k = 2
-
-    #Let's display the labels of which club each member ended up joining
-    club_labels = nx.get_node_attributes(ZKC_graph,'club')
-
-    #Getting adjacency matrix
-    A = nx.convert_matrix.to_numpy_matrix(ZKC_graph)
-    A = torch.from_numpy(A)
-
-
-    link_pred = True
-
-    if link_pred:
-        A_shape = A.shape
-        num_samples = 15
-        idx_i_test = torch.multinomial(input=torch.arange(0,float(A_shape[0])), num_samples=num_samples,
-                                       replacement=True)
-        idx_j_test = torch.tensor(zeros(num_samples)).long()
-        for i in range(len(idx_i_test)):
-            idx_j_test[i] = torch.arange(idx_i_test[i].item(), float(A_shape[1]))[torch.multinomial(input = torch.arange(idx_i_test[i].item(), float(A_shape[1])), num_samples=1, replacement=True).item()].item() #Temp solution to sample from upper corner
-        
-        #idx_j_test = torch.multinomial(input=torch.arange(0, float(A_shape[1])), num_samples=num_samples,
-        #                               replacement=True)
-        
-        A_test = A.detach().clone()
-        A_test[:] = 0
-        A_test[idx_i_test, idx_j_test] = A[idx_i_test,idx_j_test]
-        A[idx_i_test, idx_j_test] = 0
-
-    model = RAA(A=A,input_size = (N,N),k=k, sampling_weights=torch.ones(N), sample_size=20, edge_list=edge_list)
-    optimizer = torch.optim.Adam(params=model.parameters(), lr=0.01)
-    
-    losses = []
-    iterations = 10000
-    for _ in range(iterations):
-        loss = - model.log_likelihood() / model.input_size[0]
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        losses.append(loss.item())
-        print('Loss at the',_,'iteration:',loss.item())
-    
-    #Link prediction
-    if link_pred:
-        auc_score, fpr, tpr = model.link_prediction(A_test, idx_i_test, idx_j_test)
-        plt.plot(fpr, tpr, 'b', label = 'AUC = %0.2f' % auc_score)
-        plt.plot([0, 1], [0, 1],'r--', label='random')
-        plt.legend(loc = 'lower right')
-        plt.xlabel("False positive rate")
-        plt.ylabel("True positive rate")
-        plt.title("RAA model")
-        plt.show()
-
-    #Plotting latent space
-    Z = F.softmax(model.Z, dim=0)
-    G = F.sigmoid(model.G)
-    C = (Z.T * G) / (Z.T * G).sum(0)
-
-    embeddings = torch.matmul(torch.matmul(Z, C), Z).T
-    archetypes = torch.matmul(Z, C)
-
-    labels = list(club_labels.values())
-    idx_hi = [i for i, x in enumerate(labels) if x == "Mr. Hi"]
-    idx_of = [i for i, x in enumerate(labels) if x == "Officer"]
-
-    fig, (ax1, ax2) = plt.subplots(1, 2)
-    sns.heatmap(Z.detach().numpy(), cmap="YlGnBu", cbar=False, ax=ax1)
-    sns.heatmap(C.T.detach().numpy(), cmap="YlGnBu", cbar=False, ax=ax2)
-
-    if embeddings.shape[1] == 3:
-        fig = plt.figure()
-        ax = fig.add_subplot(projection='3d')
-        ax.scatter(embeddings[:,0].detach().numpy()[idx_hi], embeddings[:,1].detach().numpy()[idx_hi], embeddings[:,2].detach().numpy()[idx_hi], c = 'red', label='Mr. Hi' )
-        ax.scatter(embeddings[:,0].detach().numpy()[idx_of], embeddings[:,1].detach().numpy()[idx_of], embeddings[:,2][idx_of].detach().numpy(), c = 'blue', label='Officer')
-        ax.scatter(archetypes[0,:].detach().numpy(), archetypes[1,:].detach().numpy(), archetypes[2,:].detach().numpy(), marker = '^', c='black')
-        ax.text(embeddings[Mr_Hi,0].detach().numpy(), embeddings[Mr_Hi,1].detach().numpy(), embeddings[Mr_Hi,2].detach().numpy(), 'Mr. Hi')
-        ax.text(embeddings[John_A, 0].detach().numpy(), embeddings[John_A, 1].detach().numpy(), embeddings[John_A, 2].detach().numpy(),  'Officer')
-        ax.set_title(f"Latent space after {iterations} iterations")
-        ax.legend()
-    else:
-        fig, (ax1, ax2) = plt.subplots(1, 2)
-        ax1.scatter(embeddings[:,0].detach().numpy()[idx_hi], embeddings[:,1].detach().numpy()[idx_hi], c = 'red', label='Mr. Hi')
-        ax1.scatter(embeddings[:,0].detach().numpy()[idx_of], embeddings[:,1].detach().numpy()[idx_of], c = 'blue', label='Officer')
-        ax1.scatter(archetypes[0,:].detach().numpy(), archetypes[1,:].detach().numpy(), marker = '^', c = 'black')
-        ax1.annotate('Mr. Hi', embeddings[Mr_Hi,:])
-        ax1.annotate('Officer', embeddings[John_A, :])
-        ax1.legend()
-        ax1.set_title(f"Latent space after {iterations} iterations")
-        #Plotting learning curve
-        ax2.plot(losses)
-        ax2.set_title("Loss")
-    plt.show()
-
-
-
