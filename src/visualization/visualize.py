f776d2f32efefa85efc8bfce79d0e74244573384
ChristianDjurhuus <christian.v.djurhuus@gmail.com>
2022-05-21 22:41:15 +0200

    Adding changes to visualization code

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -48,8 +48,8 @@ class Visualization():
                         self.archetypes[2, :], marker='^', c='black')
             else:
                 fig, (ax1, ax2) = plt.subplots(1, 2)
-                ax1.scatter(embeddings[:, 0], embeddings[:, 1], c=cmap)
-                ax1.scatter(archetypes[0, :], archetypes[1, :], marker='^', c='black')
+                ax1.scatter(embeddings[:, 0], embeddings[:, 1], c=list(cmap.values()), cmap="Set2", label="Node embeddings")
+                ax1.scatter(archetypes[0, :], archetypes[1, :], marker='^', c='black', label="Archetypes")
                 # Plotting learning curve
                 if self.__class__.__name__ == "KAA":
                     ax2.plot(self.losses, c="#F2D42E")
@@ -65,7 +65,7 @@ class Visualization():
                         embeddings[:, 2], c='red')
             else:
                 fig, (ax1, ax2) = plt.subplots(1, 2)
-                ax1.scatter(embeddings[:, 0], embeddings[:, 1], c=cmap)
+                ax1.scatter(embeddings[:, 0], embeddings[:, 1], c=list(cmap.values()), cmap="Set2", label="Node embeddings")
                 ax1.legend()
                 # Plotting learning curve
                 ax2.plot(self.losses, c="#00C700")
