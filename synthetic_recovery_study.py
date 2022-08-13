9d78dbb6b88fc19586302aa2de733e0b6c80c159
ChristianDjurhuus <christian.v.djurhuus@gmail.com>
2022-08-13 11:33:56 +0200

    added code for synthetic study and altered code in model based on Nakis version

---
+++ synthetic_recovery_study.py
@@ -0,0 +1,51 @@
+import torch
+import torch.nn.functional as F
+import numpy as np
+from src.models.train_DRRAA_module import DRRAA
+from src.models.calcNMI import calcNMI
+
+
+k = 3
+d = 2
+num_init = 5
+iter = 5000
+lr = 0.01
+
+for alpha in [0.25]:#[0.25, 1, 5]:
+    dataset = f"synthetic{alpha}"
+    data = torch.from_numpy(np.loadtxt("data/train_masks/" + dataset + "/sparse_i.txt")).long()
+    data2 = torch.from_numpy(np.loadtxt("data/train_masks/" + dataset + "/sparse_j.txt")).long()
+    sparse_i_rem = torch.from_numpy(np.loadtxt("data/train_masks/" + dataset + "/sparse_i_rem.txt")).long()
+    sparse_j_rem = torch.from_numpy(np.loadtxt("data/train_masks/" + dataset + "/sparse_j_rem.txt")).long()
+    non_sparse_i = torch.from_numpy(np.loadtxt("data/train_masks/" + dataset + "/non_sparse_i.txt")).long()
+    non_sparse_j = torch.from_numpy(np.loadtxt("data/train_masks/" + dataset + "/non_sparse_j.txt")).long()
+    true_Z = torch.from_numpy(np.loadtxt("data/train_masks/" + dataset + "/true_latent_Z.txt")).float()
+
+    org_data = torch.cat((data, sparse_i_rem))
+    org_data2 = torch.cat((data2, sparse_j_rem))
+
+    NMIS = np.zeros(num_init)
+    seed = 0
+    for idx in range(num_init):
+        best_loss = 1e5
+        for _ in range(num_init):
+            model = DRRAA(data = org_data,
+                        data_2 = org_data2,
+                        k = k,
+                        d = d,
+                        data_type = "sparse",
+                        sample_size=1,
+                        seed_init = seed,
+                        link_pred = False,
+                        non_sparse_i=non_sparse_i, non_sparse_j=non_sparse_j, sparse_i_rem=sparse_i_rem, sparse_j_rem=sparse_j_rem) # Set sampling procentage size
+
+            model.train(iterations=iter, LR=lr)
+
+            if np.mean(model.losses[-10:]) < best_loss:
+                best_loss = np.mean(model.losses[-10:])
+                nmi = calcNMI(F.softmax(model.Z), true_Z).item()
+            seed += 1
+        print(nmi)
+        NMIS[idx] = nmi
+
+    np.savetxt(f'nmis_{alpha}', NMIS, delimiter=',')
